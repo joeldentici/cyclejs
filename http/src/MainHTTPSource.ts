fbe8942d3fb68b533f654d13a35204115ea371c7
Joel Dentici <joeldentici@cox.net>
2017-08-08 00:21:45 -0700

    feat(http): added monadic methods to http driver -- Adds monadic interface to @cycle/http
    This commit adds new methods to the http driver, named after the request method, to perform http
    requests in a monadic style. Rather than having effects encoded as pure instructions returned to a
    driver through a stream, effects are encoded as the results of calling a monadic function. They are
    performed when the stream resulting from the monadic function is subscribed to.

--- http/src/MainHTTPSource.ts
+++ http/src/MainHTTPSource.ts
@@ -1,9 +1,16 @@
-import {Stream, MemoryStream} from 'xstream';
+import xs, {Stream, MemoryStream} from 'xstream';
 import {HTTPSource} from './interfaces';
 import {isolateSource, isolateSink} from './isolate';
 import {DevToolEnabledSource} from '@cycle/run';
 import {adapt} from '@cycle/run/lib/adapt';
-import {Response, ResponseStream, RequestOptions} from './interfaces';
+import {
+  Response,
+  ResponseStream,
+  RequestOptions,
+  RequestInput,
+} from './interfaces';
+
+import {createResponse$, normalizeRequestInput} from './http-driver';
 
 export class MainHTTPSource implements HTTPSource {
   constructor(
@@ -26,6 +33,71 @@ export class MainHTTPSource implements HTTPSource {
     const out: DevToolEnabledSource = adapt(res$$);
     out._isCycleSource = this._name;
     return out;
+  }
+
+  public makeRequest(request: RequestInput): Stream<Response> {
+    //don't know if we should remember in the case of monadic requests
+    //as we might be better off leaving that to the user if they want it
+    const response$ = createResponse$(request).remember();
+
+    return adapt(response$);
+  }
+
+  private makeRequestMethod(
+    url: RequestInput,
+    request?: RequestInput,
+    method: string,
+  ): Stream<Response> {
+    try {
+      const options = normalizeRequestInput(url);
+
+      //if request is undefined we will treat the url as the request
+      const options2 = request === undefined
+        ? options
+        : normalizeRequestInput(request);
+
+      Object.defineProperty(options2, 'method', {
+        writable: false,
+        value: method,
+      });
+
+      //we need to set the url on the options2 if both url and request
+      //are specified
+      if (options2 !== options) {
+        Object.defineProperty(options2, 'url', {
+          writable: false,
+          value: options.url,
+        });
+      }
+
+      return this.makeRequest(options2);
+    } catch (err) {
+      return xs.throw(err);
+    }
+  }
+
+  public head(url: RequestInput, request?: RequestInput): Stream<Response> {
+    return this.makeRequestMethod(url, request, 'HEAD');
+  }
+
+  public get(url: RequestInput, request?: RequestInput): Stream<Response> {
+    return this.makeRequestMethod(url, request, 'GET');
+  }
+
+  public post(url: RequestInput, request?: RequestInput): Stream<Response> {
+    return this.makeRequestMethod(url, request, 'POST');
+  }
+
+  public put(url: RequestInput, request?: RequestInput): Stream<Response> {
+    return this.makeRequestMethod(url, request, 'PUT');
+  }
+
+  public delete(url: RequestInput, request?: RequestInput): Stream<Response> {
+    return this.makeRequestMethod(url, request, 'DELETE');
+  }
+
+  public patch(url: RequestInput, request?: RequestInput): Stream<Response> {
+    return this.makeRequestMethod(url, request, 'PATCH');
   }
 
   public isolateSource = isolateSource;
